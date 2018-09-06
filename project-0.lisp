37129210a490fea42ebd608623bfffb378687099
Jane <jlockshin@mines.edu>
2018-09-06 15:29:20 -0600

    boolean-eval

--- project-0.lisp
+++ project-0.lisp
@@ -26,9 +26,9 @@
 ;; Examples:
 ;;   (set-union '(1 2) '(2 4)) => '(1 2 4)
 (defun set-union (set-1 set-2)
-  (if (not set-2 )
+  (if (not set-2)
     set-1 
-    (if (member(car set-2) set-1)
+    (if (set-member set-1 (car set-2))
         (set-union (cdr set-2) set-1)
         (set-union (append set-1 (list (car set-2))) (cdr set-2))  
     )
@@ -43,9 +43,9 @@
 ;; Examples:
 ;;   (set-intersection '(1 2) '(2 4)) => '(2)
 (defun set-intersection (set-1 set-2)
-  (if (eq set-1 '())
-      set-1
-      (if (member (car set-1) set-2)
+  (if (not set-1)
+       set-1
+      (if (set-member set-2 (car set-1))
 	  (cons (car set-1) (set-intersection (cdr set-1) set-2))
 	  (set-intersection (cdr set-1) set-2))))
       
@@ -56,9 +56,9 @@
 ;; Examples:
 ;;   (set-diff '(1 2) '(2 4)) => '(1)
 (defun set-diff (set-1 set-2)
-  (if (eq set-1 '())
-      set-1
-      (if (member (car set-1) set-2)
+  (if (not set-1)
+       set-1
+      (if (set-member set-2 (car set-1))
 	  (set-diff (cdr set-1) set-2)
 	  (cons (car set-1) (set-diff (cdr set-1) set-2)))))
 
@@ -68,7 +68,7 @@
 ;;  (boolean-xor t nil) => t
 ;;  (boolean-xor nil nil) => nil
 (defun boolean-xor (a b)
-    (if ( eql a b)
+    (if (eql a b)
         nil
         t)
 )
@@ -78,7 +78,7 @@
 ;;  (boolean-implies t nil) => nil
 ;;  (boolean-implies nil nil) => t
 (defun boolean-implies (a b)
-    (if (eql a b )
+    (if (eql a b)
         t
         (if b
         t
@@ -92,7 +92,7 @@
 ;;  (boolean-iff t nil) => nil
 ;;  (boolean-iff nil nil) => t
 (defun boolean-iff (a b)
-    (if ( eql a b)
+    (if (eql a b)
         t
         nil)
 )
@@ -105,10 +105,15 @@
 (defun boolean-eval (exp)
   (cond
     ((equal t exp) t)
-    ((equal 'not (car exp))
-     nil)
+    ((equal 'not (car exp))  nil)
+    ((equalp (FIRST exp) (FIRST '(NOT t))) (NOT (boolean-eval (SECOND exp)) (boolean-eval (THIRD exp))))
+    ((equalp (FIRST exp) (FIRST '(AND t t))) (AND (boolean-eval (SECOND exp)) (boolean-eval (THIRD exp))))
+    ((equalp (FIRST exp) (FIRST '(OR t t))) (OR (boolean-eval (SECOND exp)) (boolean-eval (THIRD exp))))
+    ((equalp (FIRST exp) (FIRST '(XOR t))) (boolean-xor (boolean-eval (SECOND exp)) (boolean-eval (THIRD exp))))
+    ((equalp (FIRST exp) (FIRST '(IMPLIES t))) (boolean-implies (boolean-eval (SECOND exp)) (boolean-eval (THIRD exp))))
+    ((equalp (FIRST exp) (FIRST '(IFF t))) (boolean-iff (boolean-eval (SECOND exp)) (boolean-eval (THIRD exp))))
     (t
-     (TODO 'boolean-eval))))
+    nil)))
 
 ;; Perform the left fold on the list
 ;;
@@ -124,7 +129,7 @@
 ;;
 ;;   (fold-right #'- 1 '(2 3)) => 0
 (defun fold-right (function initial-value list)
-    (if  list
+    (if list
         (funcall function (car list) (fold-right function initial-value (cdr list)))
         initial-value
     )
